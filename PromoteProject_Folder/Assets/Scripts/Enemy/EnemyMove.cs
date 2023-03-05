c0a00d9a7bbcf8ff76bb7030ea7f7b798c0a1d36
WinterHana <haha920@naver.com>
2023-03-06 01:47:10 +0900

    testing...

--- PromoteProject_Folder/Assets/Scripts/Enemy/EnemyMove.cs
+++ PromoteProject_Folder/Assets/Scripts/Enemy/EnemyMove.cs
@@ -6,16 +6,14 @@ public class EnemyMove : MonoBehaviour
 {
     Rigidbody2D rigid;
     public int nextMove; // 행동지표를 결정할 변수
-
+    SpriteRenderer spriteRenderer;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         rigid.freezeRotation = true;
-        Invoke("Think", 5);     // 5초마다 Think 함수 실행
-    }
-    void Start()
-    {
-            
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Invoke("Think", 3);     // 3초마다 Think 함수 실행
     }
 
     void FixedUpdate()
@@ -27,25 +25,40 @@ public class EnemyMove : MonoBehaviour
     {
         nextMove = Random.Range(-1, 2);         // -1이면 왼쪽, 0이면 멈추기, 1이면 오른쪽으로 이동
 
-        Invoke("Think", 5);                     // 5초마다 Think 함수 실행 by 재귀함수
+        if(nextMove != 0)
+        {
+            spriteRenderer.flipX = (nextMove == 1); // nextMove가 1이면 방향 바꾸기
+        }
+
+        float nextThinkTime = Random.Range(2f, 5f);
+
+        Invoke("Think", nextThinkTime);                     // 5초마다 Think 함수 실행 by 재귀함수
     }
+
     void move()
     {
         // 한 방향으로 움직임
         rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
 
         // 플렛폼(발판) 확인
-        // Vector2 frontVec = new Vector2(rigid.position.x, rigid.position.y);
-        Debug.DrawLine(rigid.position, Vector2.down, new Color(1, 0, 0));
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.2f, rigid.position.y);
+        Debug.DrawLine(frontVec, Vector2.down, new Color(0, 1, 0));
 
-        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("tiles"));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("tiles"));
 
         if (rayHit.collider == null)
         {
-            Debug.Log("넘어진다!");
-            nextMove = nextMove * (-1);
-            CancelInvoke();
-            Invoke("Think", 5);
+            Turn();
         }
+    }
+
+    void Turn()
+    {
+        nextMove = nextMove * (-1);
+        spriteRenderer.flipX = (nextMove == 1);     // nextMove가 1이면 방향 바꾸기
+
+        CancelInvoke();
+        Invoke("Think", 2);
+
     }
 }
