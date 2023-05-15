459e1575f4a19ea59b44f2cec42b428737bcd429
WinterHana <haha920@naver.com>
2023-05-15 21:58:30 +0900

    feat : 선택창 구현 완료
    - 선택창 구현 완료했습니다.

--- PromoteProject_Folder/Assets/Scripts/UI/SelectPopup/SelectPopUpManager.cs
+++ PromoteProject_Folder/Assets/Scripts/UI/SelectPopup/SelectPopUpManager.cs
@@ -9,15 +9,24 @@ public class SelectPopUpManager : MonoBehaviour
     [Header("자식 오브젝트 삽입")]
     public GameObject panel;
     public TextMeshProUGUI text;
+    public Button accept;
+    public Button cancel;
+
     public bool sign;           // 예 아니오 선택 관련 변수
-    bool isSelect;       
-    public int num;             // 팝업 내용을 가져옴
+    public bool isSelect;       
 
     Dictionary<int, string> dic;    // 팝업창에 쓸 내용 파싱해서 저장
 
-    private static SelectPopUpManager _instance = null;
+    public delegate void YesNoCallBack();
+    public event YesNoCallBack yesCallBack;
+    public event YesNoCallBack noCallBack;
+
 
     #region 싱글톤
+
+
+    private static SelectPopUpManager _instance = null;
+
     public static SelectPopUpManager instance
     {
         get
@@ -61,21 +70,45 @@ public class SelectPopUpManager : MonoBehaviour
 
     public void OpenPopUp(int num)
     {
+        sign = false;
         isSelect = true;
         text.text = dic[num];
+
+        SetYesCallBack(() =>
+        {
+            sign = true;
+            ClosePopUp();
+        });
+
+        SetNoCallBack(() =>
+        {
+            ClosePopUp();
+        });
     }
 
-    public void closePopUp()
+    public void SetYesCallBack(YesNoCallBack listener)
+    {
+        yesCallBack += listener;
+    }
+
+    public void SetNoCallBack(YesNoCallBack listener)
+    {
+        noCallBack += listener;
+    }
+
+    public void OnYes()
+    {
+        yesCallBack?.Invoke();
+    }
+
+    public void OnNo()
+    {
+        noCallBack?.Invoke();
+    }
+
+    public void ClosePopUp()
     {
         isSelect = false;
     }
-
-    // 버튼에 적용할 함수들
-    void yes() { 
-         
-    }
-
-    void no() { 
-    
-    }
 }
+
